68019fe799113f9266ed8c3edeaa5cfbc9ae1c81
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2025-09-26 12:24:19 +0200

    add edge case test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:camino_app/main.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+void main() { 
   testWidgets('App smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const ProviderScope(child: CaminoApp()));
