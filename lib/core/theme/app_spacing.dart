4a275942b523cf0d49e197c600102df0a7e18c7b
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2025-12-02 13:08:41 +0200

    update state management

--- lib/core/theme/app_spacing.dart
+++ lib/core/theme/app_spacing.dart
@@ -35,4 +35,3 @@ class AppSpacing {
   static const BorderRadius borderRadiusLg = BorderRadius.all(Radius.circular(radiusLg));
   static const BorderRadius borderRadiusXl = BorderRadius.all(Radius.circular(radiusXl));
 }
-
