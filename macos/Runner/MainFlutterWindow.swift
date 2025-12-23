c7d33a1ac961718d60920c1318b9662b6425606a
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2025-12-23 15:52:23 +0200

    fix delegate method

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -5,8 +5,8 @@ class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
     let windowFrame = self.frame
-    self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+    self.contentViewController = flutterViewController
 
 
     RegisterGeneratedPlugins(registry: flutterViewController) 
